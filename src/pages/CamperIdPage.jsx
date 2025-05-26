199e80b57a13d4826c44040e38620f25f215797a
Verona0609 <dikaya.nika15@gmail.com>
2025-05-26 11:12:08 +0200

    added new page

---
+++ src/pages/CamperIdPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header/Header';
+
+const CamperIdPage = () => {
+  return (
+    <>
+      <Header />
+      <div className="container"></div>
+    </>
+  );
+};
+
+export default CamperIdPage;
